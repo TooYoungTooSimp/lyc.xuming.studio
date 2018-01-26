59dc79cecabfbb569f6f5f7211c8d8c0b3d3f430
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2018-01-26 19:16:58 +0800

    Fri, 26 Jan 2018 19:16:58 +0800

---
+++ post/20180125/control.js
@@ -0,0 +1,158 @@
+$(function () {
+    $.getJSON("scores.json", function (rawScore) {
+        testNames = [], ranks = [], scores = [];
+        [0, 1].forEach(function (i) {
+            ranks[i] = [];
+        });
+        [0, 1, 2, 3, 4, 5].forEach(function (i) {
+            scores[i] = [];
+        });
+        rawScore.forEach(function (singleTest) {
+            testNames.push(singleTest.name);
+            [0, 1].forEach(function (i) {
+                ranks[i].push(singleTest.rank[i]);
+            });
+            [0, 1, 2, 3, 4, 5].forEach(function (i) {
+                scores[i].push(singleTest.score[i])
+            });
+        });
+        var scoreChart = new Chart("scoreChart", {
+            type: "line",
+            data: {
+                labels: testNames,
+                datasets: [
+                    {
+                        fill: false,
+                        label: "语文",
+                        data: scores[0],
+                        yAxisID: "y150",
+                        borderColor: "red",
+                        backgroundColor: "red"
+                    },
+                    {
+                        fill: false,
+                        label: "数学",
+                        data: scores[1],
+                        yAxisID: "y150",
+                        borderColor: "green",
+                        backgroundColor: "green"
+                    },
+                    {
+                        fill: false,
+                        label: "英语",
+                        data: scores[2],
+                        yAxisID: "y150",
+                        borderColor: "pink",
+                        backgroundColor: "pink"
+                    },
+                    {
+                        fill: false,
+                        label: "物理",
+                        data: scores[3],
+                        yAxisID: "y110",
+                        borderColor: "Cyan",
+                        backgroundColor: "Cyan"
+                    },
+                    {
+                        fill: false,
+                        label: "化学",
+                        data: scores[4],
+                        yAxisID: "y100",
+                        borderColor: "purple",
+                        backgroundColor: "purple"
+                    },
+                    {
+                        fill: false,
+                        label: "生物",
+                        data: scores[5],
+                        yAxisID: "y90",
+                        borderColor: "Chartreuse",
+                        backgroundColor: "Chartreuse"
+                    },
+                    {
+                        fill: false,
+                        label: "班级排名",
+                        data: ranks[0],
+                        yAxisID: "yRank",
+                        borderColor: "black",
+                        backgroundColor: "black"
+                    },
+                    {
+                        fill: false,
+                        label: "年级排名",
+                        data: ranks[1],
+                        yAxisID: "yRank",
+                        borderColor: "DimGray",
+                        backgroundColor: "DimGray"
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [
+                        {
+                            id: "y150",
+                            type: "linear",
+                            display: false,
+                            ticks: {
+                                max: 150,
+                                min: 90
+                            }
+                        },
+                        {
+                            id: "y110",
+                            type: "linear",
+                            display: false,
+                            ticks: {
+                                max: 110,
+                                min: 66
+                            }
+                        },
+                        {
+                            id: "y100",
+                            type: "linear",
+                            display: false,
+                            ticks: {
+                                max: 100,
+                                min: 60
+                            }
+                        },
+                        {
+                            id: "y90",
+                            type: "linear",
+                            display: false,
+                            ticks: {
+                                max: 90,
+                                min: 54
+                            }
+                        },
+                        {
+                            id: "yNormalized",
+                            type: "linear",
+                            position: "left",
+                            ticks: {
+                                max: 100,
+                                min: 60,
+                                stepSize: 5
+                            },
+                            scaleLabel: {
+                                display: true,
+                                labelString: "正确率"
+                            }
+                        },
+                        {
+                            id: "yRank",
+                            type: "logarithmic",
+                            position: "right",
+                            ticks: {
+                                max: 800,
+                                min: 1,
+                                reverse: true
+                            }
+                        }
+                    ]
+                }
+            }
+        });
+    });
+});
---
+++ post/20180125/index.md
@@ -0,0 +1,13 @@
+---
+template: post_with_isso
+title: 高考直播
+pageId: 20180125
+---
+
+# 高考直播
+
+## 分数列表
+
+<canvas id="scoreChart"></canvas>
+<script src="https://cdn.bootcss.com/Chart.js/2.7.1/Chart.bundle.min.js"></script>
+<script src="control.js"></script>
